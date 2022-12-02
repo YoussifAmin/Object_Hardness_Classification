0fa9388f881d9ca0475212cb7c5fa44dcdf08c75
Youssef Amin <111621684+YoussifAmin@users.noreply.github.com>
2022-12-02 10:14:01 +0100

    Add files via upload

---
+++ C - Scripts/SLFNN/hidden files/Test_Sample.h
@@ -0,0 +1,6 @@
+#define SAMPLE_DIM 40
+#define SAMPLE_FEATURES 16
+extern float data_max[32];
+extern float data_min[32];
+extern float data[SAMPLE_DIM][SAMPLE_FEATURES];
+
---
+++ C - Scripts/SLFNN/hidden files/nn_params.h
@@ -0,0 +1,14 @@
+#define N_FC1_WT 800
+#define N_FC1_BIAS 25
+#define FC1_IN_DIM 32
+#define FC1_OUT_DIM 25
+extern float fc1_wt[N_FC1_WT];
+extern float fc1_bias[N_FC1_BIAS];
+
+#define N_FC2_WT 125
+#define N_FC2_BIAS 5
+#define FC2_IN_DIM 25
+#define FC2_OUT_DIM 5
+extern float fc2_wt[N_FC2_WT];
+extern float fc2_bias[N_FC2_BIAS];
+
